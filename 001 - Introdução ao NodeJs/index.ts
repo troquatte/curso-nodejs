ad0be0b3ccd5f02905bb463a00fc7f1de39b4ae6
Dener Troquatte <you@example.com>
2023-03-21 21:04:46 -0300

    Curso de Nodejs

---
+++ 001 - Introdução ao NodeJs/index.ts
@@ -0,0 +1,20 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+
+  if (url === "/" && method === "GET") {
+    return res.end(JSON.stringify({ method }));
+  }
+
+  if (url === "/nova-tela" && method === "GET") {
+    return res.end(JSON.stringify({ status: 200, body: "Nova tela" }));
+  }
+
+  res.statusCode = 404;
+  return res.end(JSON.stringify({ status: 404, body: "404 (Not Found)" }));
+});
+
+server.listen(3000, () => {
+  console.log("Server on");
+});
