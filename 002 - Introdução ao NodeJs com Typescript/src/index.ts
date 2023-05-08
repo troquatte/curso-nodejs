73e364ee46a51967b990966be4c59f756b057c6d
Dener Troquatte <you@example.com>
2023-05-08 09:57:28 -0300

    curso de nodejs

--- 002 - Introdução ao NodeJs com Typescript/src/index.ts
+++ 002 - Introdução ao NodeJs com Typescript/src/index.ts
@@ -1,20 +1,20 @@
-import http from "node:http";
+import http from 'node:http';
 
 const server = http.createServer((req, res) => {
   const { url, method } = req;
 
-  if (url === "/" && method === "GET") {
+  if (url === '/' && method === 'GET') {
     return res.end(JSON.stringify({ method }));
   }
 
-  if (url === "/nova-tela" && method === "GET") {
-    return res.end(JSON.stringify({ status: 200, body: "Nova tela" }));
+  if (url === '/nova-tela' && method === 'GET') {
+    return res.end(JSON.stringify({ status: 200, body: 'Nova tela' }));
   }
 
   res.statusCode = 404;
-  return res.end(JSON.stringify({ status: 404, body: "404 (Not Found)" }));
+  return res.end(JSON.stringify({ status: 404, body: '404 (Not Found)' }));
 });
 
 server.listen(3000, () => {
-  console.log("Server: ON");
+  console.log('Server: ON');
 });
