82eb0eb056958c871ac21c10b4593e17625c61d8
Dener Troquatte <you@example.com>
2023-05-09 12:32:55 -0300

    Curso de NodeJs

---
+++ 002-configurando-ts-com-nodejs/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum_1 = require("./sum");
+console.log((0, sum_1.sum)());
+//# sourceMappingURL=index.js.map
---
+++ 002-configurando-ts-com-nodejs/dist/sum.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = void 0;
+const sum = () => {
+    return 1 + 1;
+};
+exports.sum = sum;
+//# sourceMappingURL=sum.js.map
